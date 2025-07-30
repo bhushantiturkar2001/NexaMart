dd7807aff6d6285340220970b38eab7a8b13307a
bhushantiturkar2001 <titurkarbhushan@gmail.com>
2025-07-30 23:12:34 +0530

    Add SellerService interface: Define business logic for seller operations

---
+++ src/main/java/com/nexamart/service/SellerService.java
@@ -0,0 +1,28 @@
+package com.nexamart.service;
+
+import java.util.List;
+
+import com.nexamart.domain.AccountStatus;
+import com.nexamart.modal.Seller;
+
+public interface SellerService {
+
+	Seller getSellerProfile(String jwt) throws Exception;
+
+	Seller createSeller(Seller seller) throws Exception;
+
+	Seller getSellerById(Long id) throws Exception;
+
+	Seller getSellerByEmail(String email) throws Exception;
+
+	List<Seller> getAllSellers(AccountStatus status);
+
+	Seller updateSeller(Long id, Seller seller) throws Exception;
+
+	void deleteSeller(Long id) throws Exception;
+
+	Seller verifyEmail(String email, String otp) throws Exception;
+
+	Seller uodateSellerAccountStatus(Long sellerId, AccountStatus status) throws Exception;
+
+}
