785868d520e5ba7d53e1309197223b8a2b91c301
bhushantiturkar2001 <titurkarbhushan@gmail.com>
2025-07-30 23:11:44 +0530

    Update SellerRepository: Add query methods for OTP and login

--- src/main/java/com/nexamart/repository/SellerRepository.java
+++ src/main/java/com/nexamart/repository/SellerRepository.java
@@ -4,9 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nexamart.modal.Seller;
 import java.util.List;
+import com.nexamart.domain.AccountStatus;
+
 
 
 public interface SellerRepository extends JpaRepository<Seller, Long> {
 	Seller findByEmail(String email);
+	List<Seller> findByAccountStatus(AccountStatus accountStatus);
 
 }
