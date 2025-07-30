596f8e30d76b2e00f8722197079c4db3bb143f41
bhushantiturkar2001 <titurkarbhushan@gmail.com>
2025-07-30 23:12:24 +0530

    Update AuthService interface: Add OTP authentication method

--- src/main/java/com/nexamart/service/AuthService.java
+++ src/main/java/com/nexamart/service/AuthService.java
@@ -1,12 +1,13 @@
 package com.nexamart.service;
 
+import com.nexamart.domain.USER_ROLE;
 import com.nexamart.request.LoginRequest;
 import com.nexamart.response.AuthResponse;
 import com.nexamart.response.SignUpRequest;
 
 public interface AuthService {
 	
-	void sentLoginOtp(String email) throws Exception;
+	void sentLoginOtp(String email, USER_ROLE role) throws Exception;
 	String createUser(SignUpRequest req) throws Exception;
 	AuthResponse signing(LoginRequest req);
 	
