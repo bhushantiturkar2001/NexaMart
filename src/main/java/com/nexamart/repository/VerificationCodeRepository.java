4befdf75964761581e8db7b4e336cf54aae93816
bhushantiturkar2001 <titurkarbhushan@gmail.com>
2025-07-30 23:11:59 +0530

    Update VerificationCodeRepository: Improve support for OTP lookups

--- src/main/java/com/nexamart/repository/VerificationCodeRepository.java
+++ src/main/java/com/nexamart/repository/VerificationCodeRepository.java
@@ -3,11 +3,16 @@ package com.nexamart.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nexamart.modal.VerificationCode;
+import java.util.List;
+
 
 
 
 public interface VerificationCodeRepository extends JpaRepository<VerificationCode, Long> {
 	
 	VerificationCode  findByEmail(String email);
+	
+	VerificationCode findByOtp(String otp);
+	
 
 }
