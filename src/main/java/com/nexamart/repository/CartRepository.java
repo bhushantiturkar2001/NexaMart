2872c13db56c2f6c87dcc5a9b7a192e0b6a4b4e0
bhushantiturkar2001 <titurkarbhushan@gmail.com>
2025-08-23 23:30:31 +0530

    refactor(repo): adjust CartRepository query methods for new model changes

--- src/main/java/com/nexamart/repository/CartRepository.java
+++ src/main/java/com/nexamart/repository/CartRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.nexamart.modal.Cart;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
+	
+	Cart findByUserId(Long id);
 
 }
