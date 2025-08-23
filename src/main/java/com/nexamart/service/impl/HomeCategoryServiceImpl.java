1a58d2fc6176fd4b511046d60e9f254983c1cf56
bhushantiturkar2001 <titurkarbhushan@gmail.com>
2025-08-23 23:42:56 +0530

    feat(service): implement HomeCategoryService for homepage sections

---
+++ src/main/java/com/nexamart/service/impl/HomeCategoryServiceImpl.java
@@ -0,0 +1,52 @@
+package com.nexamart.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.nexamart.modal.HomeCategory;
+import com.nexamart.repository.HomeCategoryRepository;
+import com.nexamart.service.HomeCategoryService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class HomeCategoryServiceImpl implements HomeCategoryService {
+
+	private final HomeCategoryRepository homeCategoryRepository;
+
+	@Override
+	public HomeCategory createHomeCategory(HomeCategory homeCategory) {
+
+		return homeCategoryRepository.save(homeCategory);
+	}
+
+	@Override
+	public List<HomeCategory> createCategories(List<HomeCategory> homeCategories) {
+		if (homeCategoryRepository.findAll().isEmpty()) {
+			return homeCategoryRepository.saveAll(homeCategories);
+		}
+		return homeCategoryRepository.findAll();
+	}
+
+	@Override
+	public HomeCategory updateHomeCategory(HomeCategory homeCategory, Long id) throws Exception {
+		HomeCategory existingCategory = homeCategoryRepository.findById(id)
+				.orElseThrow(() -> new Exception("Category not found"));
+
+		if (homeCategory.getImage() != null) {
+			existingCategory.setImage(homeCategory.getImage());
+		}
+		if (homeCategory.getCategoryId() != null) {
+			existingCategory.setCategoryId(homeCategory.getCategoryId());
+		}
+		return homeCategoryRepository.save(existingCategory);
+	}
+
+	@Override
+	public List<HomeCategory> getAllHomeCategories() {
+		return homeCategoryRepository.findAll();
+	}
+
+}
